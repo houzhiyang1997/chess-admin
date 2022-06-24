3a1e1c39537235803f12f494d24a7259bd7ad7c1
侯智洋 <houzhiyang1997@163.com>
2022-06-24 10:56:55 +0800

    添加表单验证规则

--- src/components/Chess/AddChess.vue
+++ src/components/Chess/AddChess.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-dialog title="添加英雄" width="80%" center :model-value="centerDialogVisible" @closed="closeDialog(false)">
+  <el-dialog title="添加英雄" width="90%" center :model-value="centerDialogVisible" @closed="closeDialog(false)">
     <!-- 用户表单 -->
     <el-form
       :model="addForm"
@@ -25,7 +25,16 @@
       <el-form-item label="英雄别称" prop="displayName">
         <el-input v-model="addForm.displayName"></el-input>
       </el-form-item>
-      <el-form-item label="羁绊" prop="races">
+      <el-form-item label="英雄费率" prop="price" label-width="80px">
+        <el-select v-model="addForm.price" class="m-2" placeholder="选择英雄费用">
+          <el-option label="1费" :value="1" />
+          <el-option label="2费" :value="2" />
+          <el-option label="3费" :value="3" />
+          <el-option label="4费" :value="4" />
+          <el-option label="5费" :value="5" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="羁绊" prop="races" label-width="80px">
         <el-select
           @change="handleSelectRaces"
           multiple
@@ -40,7 +49,7 @@
       <el-form-item label="羁绊ID列表" prop="raceIds">
         <el-input v-model="addForm.raceIds" disabled></el-input>
       </el-form-item>
-      <el-form-item label="职业" prop="jobs">
+      <el-form-item label="职业" prop="jobs" label-width="80px">
         <el-select
           @change="handleSelectJobs"
           multiple
@@ -55,24 +64,7 @@
       <el-form-item label="职业ID列表" prop="jobIds">
         <el-input v-model="addForm.jobIds" disabled></el-input>
       </el-form-item>
-      <el-form-item label="英雄费率" prop="price">
-        <el-input v-model="addForm.price"></el-input>
-      </el-form-item>
-      <el-form-item label="技能" prop="skillName">
-        <el-input v-model="addForm.skillName"></el-input>
-      </el-form-item>
-      <el-form-item label="技能类型" prop="skillType">
-        <el-select v-model="addForm.skillType" class="m-2" placeholder="选择技能类型">
-          <el-option label="主动" value="主动" />
-          <el-option label="被动" value="被动" />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="技能图片地址" prop="skillImage">
-        <el-input v-model="addForm.skillImage"></el-input>
-      </el-form-item>
-      <el-form-item label="技能介绍" prop="skillDetail">
-        <el-input v-model="addForm.skillDetail" :rows="2" type="textarea"></el-input>
-      </el-form-item>
+
       <el-form-item label="生命值" prop="life">
         <el-input v-model="addForm.life"></el-input>
       </el-form-item>
@@ -103,16 +95,37 @@
       <el-form-item label="生命成长" prop="lifeData">
         <el-input v-model="addForm.lifeData"></el-input>
       </el-form-item>
-      <el-form-item label="推荐装备" prop="recEquip">
+      <el-form-item label="推荐装备" prop="recEquip" label-width="80px">
         <el-select multiple :multiple-limit="6" v-model="addForm.recEquip" class="m-2" placeholder="选择装备">
           <el-option v-for="item in equipList" :key="item.id" :label="item.name" :value="item.equipId" />
         </el-select>
       </el-form-item>
+      <el-form-item label="技能" prop="skillName">
+        <el-input v-model="addForm.skillName"></el-input>
+      </el-form-item>
+      <el-form-item label="技能类型" prop="skillType" label-width="80px">
+        <el-select v-model="addForm.skillType" class="m-2" placeholder="选择技能类型">
+          <el-option label="主动" value="主动" />
+          <el-option label="被动" value="被动" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="技能图片地址" prop="skillImage" label-width="100">
+        <el-input v-model="addForm.skillImage"></el-input>
+      </el-form-item>
+      <el-form-item label="技能介绍" prop="skillDetail">
+        <el-input v-model="addForm.skillDetail" :rows="2" type="textarea"></el-input>
+      </el-form-item>
       <el-form-item label="游戏版本" prop="version">
-        <el-input v-model="addForm.version"></el-input>
+        <el-select v-model="addForm.version" class="m-2" placeholder="选择游戏版本">
+          <el-option label="12.11" value="12.11" />
+          <el-option label="12.9" value="12.9" />
+        </el-select>
       </el-form-item>
       <el-form-item label="游戏赛季" prop="season">
-        <el-input v-model="addForm.season"></el-input>
+        <el-select v-model="addForm.season" class="m-2" placeholder="选择游戏赛季">
+          <el-option label="S7-巨龙之境" value="2022.S7" />
+          <el-option label="S7-霓虹之夜" value="2022.S6" />
+        </el-select>
       </el-form-item>
     </el-form>
     <template #footer>
@@ -185,25 +198,138 @@ export default {
     })
     // 校验规则
     const addFormRules = {
-      username: [
-        { required: true, message: '请输入用户名', trigger: 'blur' },
-        { min: 3, max: 10, message: '用户名长度在3~10个字符之间', trigger: 'blur' }
+      chessId: [
+        { required: true, message: '请输入棋子ID', trigger: 'blur' },
+        {
+          pattern: /^[0-9]{1,5}$/,
+          message: '请输入1到5位数字',
+          trigger: 'blur'
+        }
       ],
-      password: [
-        { required: true, message: '请输入密码', trigger: 'blur' },
-        { min: 6, max: 15, message: '密码长度在6~15个字符之间', trigger: 'blur' }
+      TFTID: [
+        { required: true, message: '请输入云顶通用ID', trigger: 'blur' },
+        {
+          pattern: /^[0-9]{1,5}$/,
+          message: '请输入1到5位数字',
+          trigger: 'blur'
+        }
       ],
-      nickName: [
-        { required: true, message: '请输入昵称', trigger: 'blur' },
-        { min: 3, max: 10, message: '昵称长度在3~10个字符之间', trigger: 'blur' }
+      title: [
+        { required: true, message: '请输入英雄名', trigger: 'blur' },
+        { min: 3, max: 10, message: '英雄名长度在3~10个字符之间', trigger: 'blur' }
       ],
-      level: [
-        { required: true, message: '请输入等级', trigger: 'blur' },
-        { min: 3, max: 4, message: '等级格式为LV1-10', trigger: 'blur' }
+      name: [
+        { required: true, message: '请输入图片Name', trigger: 'blur' },
+        {
+          pattern: /^[0-9]{1,5}\.(png|jpg)$/,
+          message: '请输入123.png/jpg格式的输入',
+          trigger: 'blur'
+        }
       ],
-      exp: [{ required: true, message: '请输入经验值默认为0', trigger: 'blur' }],
-      score: [{ required: true, message: '请输入积分默认为0', trigger: 'blur' }],
-      imgUrl: [{ required: true, message: '请输入头像地址', trigger: 'blur' }]
+      displayName: [
+        { required: true, message: '请输入英雄别称', trigger: 'blur' },
+        { min: 1, max: 10, message: '英雄别称长度在1~10个字符之间', trigger: 'blur' }
+      ],
+      price: [{ required: true, message: '请选择费率', trigger: 'blur' }],
+      races: [{ required: true, message: '请选择羁绊', trigger: 'blur' }],
+      jobs: [{ required: true, message: '请选择职业', trigger: 'blur' }],
+      life: [
+        { required: true, message: '请输入生命值', trigger: 'blur' },
+        {
+          pattern: /^[0-9]{1,5}$/,
+          message: '请输入1到5位数字',
+          trigger: 'blur'
+        }
+      ],
+      magic: [
+        { required: true, message: '请输入法力值', trigger: 'blur' },
+        {
+          pattern: /^[0-9]{1,3}$/,
+          message: '请输入1到3位数字',
+          trigger: 'blur'
+        }
+      ],
+      startMagic: [
+        { required: true, message: '请输入初始法力值', trigger: 'blur' },
+        {
+          pattern: /^[0-9]{1,3}$/,
+          message: '请输入1到3位数字',
+          trigger: 'blur'
+        }
+      ],
+      armor: [
+        { required: true, message: '请输入护甲', trigger: 'blur' },
+        {
+          pattern: /^[0-9]{1,3}$/,
+          message: '请输入1到3位数字',
+          trigger: 'blur'
+        }
+      ],
+      spellBlock: [
+        { required: true, message: '请输入魔抗', trigger: 'blur' },
+        {
+          pattern: /^[0-9]{1,3}$/,
+          message: '请输入1到3位数字',
+          trigger: 'blur'
+        }
+      ],
+      attack: [
+        { required: true, message: '请输入攻击力', trigger: 'blur' },
+        {
+          pattern: /^[0-9]{1,5}$/,
+          message: '请输入1到5位数字',
+          trigger: 'blur'
+        }
+      ],
+      attackSpeed: [
+        { required: true, message: '请输入攻击速度', trigger: 'blur' },
+        {
+          pattern: /^[0-9]\.[0-9]{2,3}$/,
+          message: '请输入类似0.70的格式',
+          trigger: 'blur'
+        }
+      ],
+      attackRange: [
+        { required: true, message: '请输入攻击距离', trigger: 'blur' },
+        {
+          pattern: /^[0-9]{3,4}$/,
+          message: '请输入3到4位数字',
+          trigger: 'blur'
+        }
+      ],
+      attackData: [
+        { required: true, message: '请输入攻击成长', trigger: 'blur' },
+        {
+          pattern: /^[0-9]{1,4}\/[0-9]{1,4}\/[0-9]{1,4}$/,
+          message: '请输入类似1000/2000/3000的格式',
+          trigger: 'blur'
+        }
+      ],
+      lifeData: [
+        { required: true, message: '请输入生命成长', trigger: 'blur' },
+        {
+          pattern: /^[0-9]{1,4}\/[0-9]{1,4}\/[0-9]{1,4}$/,
+          message: '请输入类似1000/2000/3000的格式',
+          trigger: 'blur'
+        }
+      ],
+      recEquip: [{ required: true, message: '请选择推荐装备', trigger: 'blur' }],
+      skillName: [
+        { required: true, message: '请输入技能名称', trigger: 'blur' },
+        { min: 2, max: 12, message: '技能名称长度为2-12', trigger: 'blur' }
+      ],
+      skillType: [{ required: true, message: '请选择技能类型', trigger: 'blur' }],
+      skillImage: [
+        { required: true, message: '请输入技能图片地址', trigger: 'blur' },
+        {
+          pattern: /^(http:|https:).*\.(png|jpg)$/,
+          message: '请输入正确的地址',
+          trigger: 'blur'
+        }
+      ],
+      skillDetail: [{ required: true, message: '请输入技能介绍', trigger: 'blur' }],
+      version: [{ required: true, message: '请选择游戏版本', trigger: 'blur' }],
+      season: [{ required: true, message: '请选择游戏赛季', trigger: 'blur' }]
     }
     // 关闭对话框
     const closeDialog = visible => {
